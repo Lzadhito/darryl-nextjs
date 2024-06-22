b10ba2f00ba5cf9cf1b3ae19544d5985611c6144
lzadhito <lzadhito@gmail.com>
2024-06-22 10:08:44 +0700

    feat: init branch adi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 export default function Homepage() {
-  return <div>Aa Rizky</div>;
+  return <div className="bg-blue-300 h-screen flex items-center justify-center text-6xl font-bold">Lebah Adi</div>;
 }
